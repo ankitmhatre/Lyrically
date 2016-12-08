b190906cef409e08e0ab2f88b268d121ac037321
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2016-12-09 01:43:47 +0530

    get rid of numerous nests (if/else) may have some errors!

--- app/src/main/java/com/shkmishra/lyrically/LyricsService.java
+++ app/src/main/java/com/shkmishra/lyrically/LyricsService.java
@@ -54,7 +54,7 @@ import java.util.ArrayList;
 
 public class LyricsService extends Service {
 
-
+static Element element;
     String title, lyrics;
     boolean offlineMusic = false;
     File[] lyricsFiles;
@@ -458,96 +458,142 @@ public class LyricsService extends Service {
 
         boolean found = true;
 
-        String url = "s", lyricURL = "s";
+        String url = "s", lyricURL = "s", page, temp=null;
 
 
         @Override
         protected Object doInBackground(Object[] params) {
 
 
-            try {
+             try {
+            url = "https://www.google.com/search?q=" + URLEncoder.encode(artistU + " " + trackU + " lyrics", "UTF-8");
 
-                url = "https://www.google.com/search?q=" + URLEncoder.encode("lyrics+azlyrics+" + artistU + "+" + trackU, "UTF-8");
-                Document document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(10000).get();
-                Element results = document.select("h3.r > a").first();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            url = "https://www.google.com/search?q=lyrics+" + artistU + "+" + trackU;
+        }
+        // Getting url and connecting
+        try {
+   
+            Document document = Jsoup.connect(url).userAgent(Net.USER_AGENT).timeout(10000).get();
+            Elements results = document.select("h3.r > a");
+           
+            results = results.select("a");
+            //.substring(7, results.attr("href").indexOf("&"));
+          
 
-                lyricURL = results.attr("href").substring(7, results.attr("href").indexOf("&"));
-                Element element;
-                String temp;
-
+//Check the procider now
+            for (Element element : results.subList(0, 4)) {
+                
+                lyricURL = element.attr("href");
+                Log.d("thedatahrefurl", lyricURL);
                 if (lyricURL.contains("azlyrics.com/lyrics")) {
-                    document = Jsoup.connect(lyricURL).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").get();
-                    title = artist + " - " + track;
+                    document = Jsoup.connect(lyricURL).userAgent(Net.USER_AGENT).get();
+
                     String page = document.toString();
 
                     page = page.substring(page.indexOf("that. -->") + 9);
                     page = page.substring(0, page.indexOf("</div>"));
                     temp = page;
+                    
+                    break;
 
-                } else {
+                } else if (lyricURL.contains("genius")) {
 
-                    url = "https://www.google.com/search?q=" + URLEncoder.encode("genius+" + artistU + "+" + trackU + "lyrics", "UTF-8");
-                    document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(10000).get();
+                    document = Jsoup.connect(lyricURL).userAgent(Net.USER_AGENT).get();
+                    track = document.select("meta[property=og:title]").first().attr("content");
 
-                    results = document.select("h3.r > a").first();
-                    lyricURL = results.attr("href").substring(7, results.attr("href").indexOf("&"));
+                    Elements selector = document.select("div.h2");
 
-
-                    if (lyricURL.contains("genius")) {
-
-                        document = Jsoup.connect(lyricURL).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").get();
-                        title = document.select("meta[property=og:title]").first().attr("content");
-
-                        Elements selector = document.select("div.h2");
-
-                        for (Element e : selector) {
-                            e.remove();
-                        }
-
-                        element = document.select("div[class=song_body-lyrics]").first();
-                        temp = element.toString().substring(0, element.toString().indexOf("</lyrics>"));
-                    } else {
-
-                        url = "https://www.google.com/search?q=" + URLEncoder.encode("lyrics.wikia+" + trackU + "+" + artistU, "UTF-8");
-
-
-                        document = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(10000).get();
-
-                        results = document.select("h3.r > a").first();
-                        lyricURL = results.attr("href").substring(7, results.attr("href").indexOf("&"));
-
-
-                        document = Jsoup.connect(lyricURL).userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36").get();
-                        title = document.select("meta[property=og:title]").first().attr("content");
-                        title = title.replace(":", " - ");
-
-                        element = document.select("div[class=lyricbox]").first();
-                        temp = element.toString();
+                    for (Element e : selector) {
+                        e.remove();
                     }
 
+                    element = document.select("div[class=song_body-lyrics]").first();
+                    temp = element.toString().substring(0, element.toString().indexOf("</lyrics>"));
+               
+                    break;
+                } else if (lyricURL.contains("lyrics.wikia")) {
+
+                    document = Jsoup.connect(lyricURL).userAgent(Net.USER_AGENT).get();
+                    track = document.select("meta[property=og:title]").first().attr("content");
+                    //track = track.replace(":", " - ");
+
+                    element = document.select("div[class=lyricbox]").first();
+                    temp = element.toString();
+                   
+                    break;
+                } else if (lyricURL.contains("glamsham.com")) {
+
+                    document = Jsoup.connect(lyricURL).userAgent(Net.USER_AGENT).get();
+                    track = document.select("meta[property=og:title]").first().attr("content");
+                    //track = track.replace(":", " - ");
+
+                    Element e = document.select("div[class=col-sm-6]").first();
+                    temp = e.select("font").text();
+                   
+                    break;
+
+                } else if (lyricURL.contains("lyricsmint.com")) {
+                    document = Jsoup.connect(lyricURL).userAgent(Net.USER_AGENT).get();
+                    track = document.select("meta[property=og:title]").first().attr("content");
+                    //track = track.replace(":", " - ");
+
+                    Element e = document.select("div#lyric").first();
+                    temp = e.text();
+               
+                    break;
+                } else if (lyricURL.contains(("lyricskatta.com"))) {
+                    String blank = "";
+                    document = Jsoup.connect(lyricURL).userAgent(Net.USER_AGENT).get();
+                    //track
+                    Element e = document.select("div[class=entry-inner]").first();
+                    Elements e2 = e.getElementsByTag("p");
+                    for (Element e3 : e2) {
+                        blank = blank + "\n" + e3.text();
+
+                    }
+                    temp = blank;
+                   
+                    break;
 
                 }
-
-                temp = temp.replaceAll("(?i)<br[^>]*>", "br2n");
-                temp = temp.replaceAll("]", "]shk");
-                temp = temp.replaceAll("\\[", "shk[");
-
-
-                lyrics = Jsoup.parse(temp).text();
-                lyrics = lyrics.replaceAll("br2n", "\n");
-                lyrics = lyrics.replaceAll("]shk", "]\n");
-                lyrics = lyrics.replaceAll("shk\\[", "\n [");
-                if (lyricURL.contains("genius"))
-                    lyrics = lyrics.substring(lyrics.indexOf("Lyrics") + 6);
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                found = false;
+                
+                ///ADD more search Providers in the else statements
             }
-            return null;
+               
+
+
+            if (temp == null)
+                return new Lyrics(Lyrics.NO_RESULT);
+
+            temp = Utilities.br2nl(temp);
+
+            temp = temp.replaceAll("(?i)<br[^>]*>", "br2n");
+            temp = temp.replaceAll("]", "]shk");
+            temp = temp.replaceAll("\\[", "shk[");
+            temp = temp.replaceAll("br2n", "\n");
+
+
+            lyrics = Jsoup.parse(temp).text();
+            lyrics = lyrics.replaceAll("br2n", "\n");
+            lyrics = lyrics.replaceAll("]shk", "]\n");
+            lyrics = lyrics.replaceAll("shk\\[", "\n [");
+
+          
+
+           
+            return lyrics;
+        } catch (Exception e) {
+         
+            e.printStackTrace();
+            return null
         }
+
+    }
+
+
+}
 
 
         @Override
